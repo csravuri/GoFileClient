c6f482ea2cb72f3047e42e26264cf3fefa757b7d
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-11-26 21:04:05 +0530

    service models to new class

--- GoFile/GoFile.Client/Services/Entity.cs
+++ GoFile/GoFile.Client/Services/Entity.cs
@@ -1,6 +1,28 @@
 ﻿namespace GoFile.Client.Services
 {
-	internal class Entity
+	public class ServiceResponse<T>
 	{
+		public string status { get; set; }
+		public T data { get; set; }
+	}
+
+	public class TokenDetails
+	{
+		public string token { get; set; }
+	}
+
+	public class AccountDetails
+	{
+		public string id { get; set; }
+		public string email { get; set; }
+		public string tier { get; set; }
+		public string token { get; set; }
+		public string rootFolder { get; set; }
+		public int filesCount { get; set; }
+		public int totalSize { get; set; }
+		public int total30DDLTraffic { get; set; }
+		public int credit { get; set; }
+		public string currency { get; set; }
+		public string currencySign { get; set; }
 	}
 }
